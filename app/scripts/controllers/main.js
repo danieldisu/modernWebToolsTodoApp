32254a398f463388012ed3ddf22b9a0e5322a91b
ddiaz <ddiaz@autentia.com>
2013-10-04 14:51:47 +0200

    Resueltos problemas con indentacion y desactivados los test

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,35 +3,35 @@
 angular.module('webToolsApp').controller('MainCtrl', function ($scope) {
 
     var task1 = {
-        text : "Esto es una tarea de prueba",
-        date : Date.now(),
-        done : false
-    }
+        text: 'Esto es una tarea de prueba',
+        date: Date.now(),
+        done: false
+    };
 
     var task2 = {
-        text : "esto es otra tarea",
-        date : Date.now(),
-        done : false
-    }
+        text: 'esto es otra tarea',
+        date: Date.now(),
+        done: false
+    };
 
-    $scope.tasks = [task1,task2];
+    $scope.tasks = [task1, task2];
 
-    $scope.addTodo = function() {
+    $scope.addTodo = function () {
         var task = {
-            text : $scope.newTaskText,
-            date : Date.now(),
-            done : false
-        }
+            text: $scope.newTaskText,
+            date: Date.now(),
+            done: false
+        };
 
         $scope.tasks.push(task);
 
-        $scope.newTaskText = "";
+        $scope.newTaskText = '';
 
-    }
+    };
 
-    $scope.delete = function(task){
+    $scope.delete = function (task) {
         var index = $scope.tasks.indexOf(task);
-        $scope.tasks.splice(index,1);
-    }
+        $scope.tasks.splice(index, 1);
+    };
 
 });
